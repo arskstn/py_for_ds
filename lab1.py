ececb013f94d2434e6c4c6115b2a697e0ff78146
arskstn <arskstn@gmail.com>
2024-10-11 18:22:49 +0700

    Fixed lab1

--- lab1.py
+++ lab1.py
@@ -25,12 +25,16 @@ def number_menu():
         choice = int(input("Выберите операцию с числами:\n1 - Сложение\n2 - Вычитание\n3 - Умножение\n4 - Деление\n5 - Выход в меню\nВведите номер операции: "))
         if (choice == 1):
             print("Результат:",float(input("Введите первое число: ")) + float(input("Введите второе число: ")))
+            number_menu()
         elif (choice == 2):
             print("Результат:",float(input("Введите первое число: ")) - float(input("Введите второе число: ")))
+            number_menu()
         elif (choice == 3):
             print("Результат:",float(input("Введите первое число: ")) * float(input("Введите второе число: ")))
+            number_menu()
         elif (choice == 4):
             print("Результат:",float(input("Введите первое число: ")) / float(input("Введите второе число: ")))
+            number_menu()
         elif (choice == 5):
             menu()
         else:
@@ -81,16 +85,16 @@ def matrix_menu():
             if(j==k):
                 m1 = [[float(input(f"Задайте элемент на строке {row + 1}, столбце {col + 1}: ")) for col in range(j)] for row in range(i)]
                 m2 = [[float(input(f"Задайте элемент на строке {row + 1}, столбце {col + 1}: ")) for col in range(l)] for row in range(k)]
-                m3 = [[0] * i for _ in range(i)]
+                m3 = [[0] * l for _ in range(i)]
                 for i in range(len(m1)):
                     for j in range(len(m2[0])):
-                        for k in range(len(m2)):
+                        for k in range(len(m2)):  # Проходим по строкам матрицы m2
                             m3[i][j] += m1[i][k] * m2[k][j]
                 print(*m3, sep='\n')
                 time.sleep(2)
                 matrix_menu()            
             else:
-                print("Кол-во столбцов первой матрицы и кол-во строк второй матрицы должны быть одинаковы, попробуйте еще раз")
+                print("Размерности при умножении должны удовлетворять правилам умножения, попробуйте еще раз")
                 time.sleep(2)
                 matrix_menu()
         elif (choice == 4):
